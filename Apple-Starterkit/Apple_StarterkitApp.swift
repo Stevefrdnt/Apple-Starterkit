724f305243d7908848a344890236fe11f5d5b756
Steven Ferdianto <steven.ferdianto0110@gmail.com>
2024-04-16 13:40:05 +0700

    Initial Commit

---
+++ Apple-Starterkit/Apple_StarterkitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_StarterkitApp.swift
+//  Apple-Starterkit
+//
+//  Created by steven ferdianto on 16/04/24.
+//
+
+import SwiftUI
+
+@main
+struct Apple_StarterkitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
